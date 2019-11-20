e6a7273defc0be7c2de7e27be4e57dba2184f567
Augies <augies@cox.net>
2019-11-19 18:20:52 -0600

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,76 @@
+#include "../include/main.h"
+
+//Initialize all Variables here
+pros::Controller controller(CONTROLLER_MASTER);
+
+/**
+ * Call this function to set rotation readings of the motors to 0
+ * Makes it easier to do autonomous modes and generally understand how a bot is moving
+ */
+ void tareMotors(){
+	 //tare_position is the function used to reset the amount of rotations. Do this to every motor
+	 //DriveTrainL.tare_position();
+ }
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+  tareMotors(); //We want it to start at 0 rotations each time our bot is restarted
+}
+
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+	//Setup goes here
+
+	while (true) {
+		//Functions and control go here
+	}
+}
